72255d44fe31d4799f57a3b50771933850a38839
AceWolf <koumaar.jayashankar@gmail.com>
2018-11-12 18:02:57 +0100

    Created shapes, spawner and movement

---
+++ Tetris/Assets/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.Q))
+        {
+            if (transform.position.x > -3.1)
+            {
+                transform.position += new Vector3(-0.2f, 0);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            if (transform.position.x < 3.4)
+            {
+                transform.position += new Vector3(0.2f, 0);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -0.2f);
+        }
+    }
+}
---
+++ Tetris/Assets/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] TetrisShapes;
+
+    private bool canSpawn = false;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(TetrisShapes[Random.Range(0, 6)],transform.position+new Vector3(0,-1,0),Quaternion.identity);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (canSpawn)
+        {
+            Instantiate(TetrisShapes[Random.Range(0, 6)], transform.position + new Vector3(0, -1, 0), Quaternion.identity);
+        }
+	}
+}
